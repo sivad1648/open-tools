5907a911581d2787a18406bc5db41d8def91a191
sivad1648 <95473996+sivad1648@users.noreply.github.com>
2022-06-14 18:47:53 +0530

    Update provider.tf

--- terraform/eks/networking-eks/provider.tf
+++ terraform/eks/networking-eks/provider.tf
@@ -3,22 +3,7 @@ provider "aws" {
   shared_credentials_file = var.credentials_file
   profile                 = var.profile
   region                  = var.region
-  version                 = ">= 2.61.0"
+  
 }
 
-provider "random" {
-  version = ">= 2.2.1"
-}
-
-provider "local" {
-  version = ">= 1.4.0"
-}
-
-provider "template" {
-  version = ">= 2.1.2"
-}
-
-provider "null" {
-  version = ">= 2.1.2"
-}
 
